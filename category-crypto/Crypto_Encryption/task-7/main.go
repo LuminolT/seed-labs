559ab0a6d2e08349e278b980b978f74ea668f36e
LuminolT <luminol.chen@gmail.com>
2022-12-30 00:17:04 +0800

    format: format lab2-7

--- category-crypto/Crypto_Encryption/task-7/main.go
+++ category-crypto/Crypto_Encryption/task-7/main.go
@@ -1,8 +1,8 @@
 /*
  * @Author: LuminolT copper_sulfate@qq.com
  * @Date: 2022-12-24 22:32:56
- * @LastEditors: LuminolT copper_sulfate@qq.com
- * @LastEditTime: 2022-12-27 18:06:19
+ * @LastEditors: LuminolT luminol.chen@gmail.com
+ * @LastEditTime: 2022-12-29 13:50:47
  * @FilePath: /Lab-2-Crpyto-Sym/task-7/main.go
  * @Description: a go script to crack the AES-CBC-128 encryption
  *
@@ -22,6 +22,59 @@ import (
 
 	mopenssl "github.com/microsoft/go-crypto-openssl/openssl"
 )
+
+/**
+ * @description: main function
+ * @return {*}
+ */
+func main() {
+	// initalization of the openssl
+	mopenssl.Init()
+
+	// initial data: plaintext, ciphertext and iv
+	text_p := []byte("This is a top secret.")
+	text_c, err := hex.DecodeString("764aa26b55a4da654df6b19e4bce00f4ed05e09346fb0e762583cb7da2ac93a2")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// plain text padding with PKCS7
+	text_p = PKCS7Padding(text_p, 16)
+
+	iv, err := hex.DecodeString("aabbccddeeff00998877665544332211")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// read file ./words.txt
+	file, err := os.Open("words.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var wg sync.WaitGroup
+
+	for scanner.Scan() {
+		// get the probable key
+		key := []byte(scanner.Text())
+		wg.Add(1)
+		go func() {
+			flag, key_pad, err := CrackCBC(key, text_c, text_p, iv)
+			if err != nil {
+				fmt.Println(err)
+			}
+			if flag {
+				fmt.Println("key: ", string(key_pad))
+			}
+		}()
+	}
+	wg.Wait()
+}
 
 /**
 * @description: pad ciphertext with PKCS7
@@ -78,57 +131,4 @@ func CrackCBC(key []byte, text_c []byte, text_p []byte, iv []byte) (bool, []byte
 	} else {
 		return false, nil, nil
 	}
-}
-
-/**
- * @description: main function
- * @return {*}
- */
-func main() {
-	// initalization of the openssl
-	mopenssl.Init()
-
-	// initial data: plaintext, ciphertext and iv
-	text_p := []byte("This is a top secret.")
-	text_c, err := hex.DecodeString("764aa26b55a4da654df6b19e4bce00f4ed05e09346fb0e762583cb7da2ac93a2")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// plain text padding with PKCS7
-	text_p = PKCS7Padding(text_p, 16)
-
-	iv, err := hex.DecodeString("aabbccddeeff00998877665544332211")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// read file ./words.txt
-	file, err := os.Open("words.txt")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	var wg sync.WaitGroup
-
-	for scanner.Scan() {
-		// get the probable key
-		key := []byte(scanner.Text())
-		wg.Add(1)
-		go func() {
-			flag, key_pad, err := CrackCBC(key, text_c, text_p, iv)
-			if err != nil {
-				fmt.Println(err)
-			}
-			if flag {
-				fmt.Println("key: ", string(key_pad))
-			}
-		}()
-	}
-	wg.Wait()
 }
